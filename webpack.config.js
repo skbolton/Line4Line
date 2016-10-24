169db72a14c21306d64afb8334680614ea2a98c3
skbolton <stephenkbolton@gmail.com>
2016-10-24 18:48:51 -0500

    Adjust webpack setup to be closer to our needs.

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
-var path = require('path')
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const webpack = require('webpack');
+const merge = require('webpack-merge');
 
-
-
-module.exports = {
+// common config across dev and prod setups
+const common = {
   entry: path.join(__dirname, 'client'),
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'server', 'public'),
     filename: 'bundle.js'
   },
 
@@ -16,12 +17,6 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
-        query: {
-          presets: [
-            'es2015',
-            'react'
-          ]
-        }
       },
 
       { 
@@ -43,11 +38,50 @@ module.exports = {
 
   plugins: [
     new ExtractTextPlugin('styles.css')
-  ],
+  ]
+};
 
-  node: {
-    fs: 'empty',
-    net: 'empty',
-    tls: 'empty'
+// config for a dev setup
+const devConfig = {
+  devtool: 'source-maps',
+  devServer: {
+    inline: true,
+    hot: true,
+    historyApiFallback: true
   }
+};
+
+const prodConfig = {
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      beautify: false,
+      comments: false,
+      compress: {
+        warnings: false,
+        // drop_console: true
+      },
+      mangle: {
+        except: ['$'],
+        screw_ie8: true,
+        keep_fnames: true
+      }
+    })
+  ]
+};
+
+// export config for prod or dev based on npm 
+// command ran
+// npm run dev would do development build
+// npm run build would do production build
+const target = process.env.npm_lifecycle_event;
+switch (target) {
+  case 'dev' :
+    module.exports = merge(common, devConfig);
+    break;
+  default :
+    module.exports = merge(common, prodConfig);
+    break;
 }
+
+
+
