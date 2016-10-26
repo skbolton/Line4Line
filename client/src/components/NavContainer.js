15fe8f907e84e7691362a0c8d5f3428e4496cc2f
Michael Lefkowitz <lefkowitz.michael@gmail.com>
2016-10-26 16:05:51 -0500

    add sesh for joseph

--- client/src/components/NavContainer.js
+++ client/src/components/NavContainer.js
@@ -1,10 +1,12 @@
-import React from 'react'
+// import React from 'react'
+//
+// const NavContainer = (props) => {
+//   return (
+//     <div>
+//       <Nav />
+//       {props.children}
+//     </div>
+//   )
+// }
 
-const NavContainer = (props) => {
-  return (
-    <div>
-      <Nav />
-      {props.children}
-    </div>
-  )
-}
+// ^ i don't think this is used
