56295d78e79430ca61bd7c515f93684aeeba50a9
Michael Lefkowitz <lefkowitz.michael@gmail.com>
2016-10-29 12:22:40 -0500

    one

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -27,9 +27,10 @@ router.route('/stories/:id').get(isAuthed, stories.joinStory, stories.getOneStor
 router.route('/stories').post(isAuthed, stories.createStory)
 
 router.route('/stories/:id').put((req, res) => {
-
   if (req.query.vote) {
-    stories.votingFunction(req.query.vote, req.params.id, req.session.passport.user._id)
+    stories.votingFunction(req.query.vote, req.params.id, req.session.passport.user._id).then(voteCount =>{
+      res.json({votes: voteCount})
+    })
   } else {
     // this is never actually called as it's being called on the socket
     stories.createNewLine
