4403853ba399f4122e4f581fd1d8e55187c6528d
skbolton <stephenkbolton@gmail.com>
2016-10-29 10:46:14 -0500

    Add new welcome screen message.

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -5,7 +5,9 @@ const Welcome = (props) => {
     <div className="container">
       <div className="row">
         <div className="col-xs-12">
-          Here we will explain the app
+          <p>
+          Welcome to Line After Line, a place where the stories never go how you expect! Playing the game involves taking turns adding a line to an unfolding story, only knowing the line that was written before you. Once completed the story is printed out for all players to see. You can even upvote and share popular stories with your friends. Gather around and let hilarity ensue!
+          </p>
         </div>
       </div>
     </div>
